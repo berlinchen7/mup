Figured out the correct scaling for inproj+B in mamba

examples/SSMs/main.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import time
+from zoneinfo import ZoneInfo
 from datetime import datetime
 
 import numpy as np
@@ -12,7 +13,8 @@ import torch.optim as optim
 try:
     from apex import amp
 except:
-    print('Failed to import apex. You can still train with --precision {float|double}.')
+    pass
+    # print('Failed to import apex. You can still train with --precision {float|double}.')
 
 from mup.coord_check import get_coord_data, plot_coord_data
 from mup import MuAdam, MuSGD, get_shapes, make_base_shapes, set_base_shapes
@@ -21,7 +23,7 @@ import examples.SSMs.data as data
 from examples.SSMs.mixer_seq_simple import MixerModelWithSimpleHead
 from examples.SSMs.ssm import set_base_shapes_custom
 
-torch.set_default_device('cuda')
+# torch.set_default_device('cuda:2')
 
 ###############################################################################
 # Training code
@@ -81,7 +83,7 @@ def coord_check(mup, lr, optimizer, batch_size, nsteps, nseeds, data_dir, args, 
                         n_layer = n_layer,
                         vocab_size = ntokens,
                         d_intermediate = 0,
-                        ssm_cfg=None,
+                        ssm_cfg={'norm': args.coord_check_norm},
                         device=None,
                         dtype=None,
                         
@@ -93,7 +95,7 @@ def coord_check(mup, lr, optimizer, batch_size, nsteps, nseeds, data_dir, args, 
                         n_layer = n_layer,
                         vocab_size = ntokens,
                         d_intermediate = 0,
-                        ssm_cfg=None,
+                        ssm_cfg={'norm': args.coord_check_norm},
                         device=None,
                         dtype=None,
                         
@@ -113,7 +115,7 @@ def coord_check(mup, lr, optimizer, batch_size, nsteps, nseeds, data_dir, args, 
         return f
 
     optimizer = optimizer.replace('mu', '')
-    widths = 2**np.arange(7, 13) # 14) #2**np.arange(7, 14 if optimizer=='sgd' else 12)
+    widths = 2**np.arange(7, 14) # 14) #2**np.arange(7, 14 if optimizer=='sgd' else 12)
     models = {w: gen(w, standparam=not mup) for w in widths}
 
     
@@ -122,13 +124,12 @@ def coord_check(mup, lr, optimizer, batch_size, nsteps, nseeds, data_dir, args, 
     df = get_coord_data(models, batchloader(train_data, args.bptt), mup=mup, lr=lr, optimizer=optimizer, 
                         flatten_output=True, nseeds=nseeds, nsteps=nsteps, lossfn='nll', cuda=True,
                         hyperparam_mode=args.hyperparam_mode)
-    # print(optimizer)
     # if mup and optimizer=='adam':
     #     df.to_pickle("/home/berlin/mup/examples/Transformer/coord_checks/μp_trsfmr_adam_coord.pkl")  
 
     prm = args.hyperparam_mode #'μP' if mup else 'SP'
     
-    timestamp = datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
+    timestamp = datetime.now(ZoneInfo("America/New_York")).strftime('%m_%d_%Y_%H_%M_%S')
     exp_name = f'coordcheck_ssmmixer_{args.hyperparam_mode}_{timestamp}'
     # df.to_pickle(f"{plotdir}/{exp_name}.pkl")  
     df.to_csv(f"{plotdir}/{exp_name}.csv") 
@@ -136,13 +137,13 @@ def coord_check(mup, lr, optimizer, batch_size, nsteps, nseeds, data_dir, args, 
     return plot_coord_data(df, legend=legend,
         save_to=os.path.join(plotdir, f'{exp_name}.png'),
         suptitle=f'{prm} SSM {optimizer} lr={lr} nseeds={nseeds}',
-        face_color='xkcd:light grey' if not mup else None)
+        face_color='xkcd:light grey' if not mup else None,)
 
 
 if __name__ == '__main__':
 
     import os
-    os.chdir('/n/fs/scratch/bc2188/mup/examples/SSMs') # NOTE Added by BC on Aug 14, 2024
+    os.chdir('/home/berlin/mup/examples/SSMs') # NOTE Added by BC on Aug 14, 2024
 
     parser = argparse.ArgumentParser(description=
     '''
@@ -232,11 +233,16 @@ if __name__ == '__main__':
     parser.add_argument('--deferred_init', action='store_true', help='Skip instantiating the base and delta models for mup. Requires torchdistx.')
     parser.add_argument('--hyperparam_mode', type=str, help='hyperparam_mode.')
     parser.add_argument('--d_model_base', type=int, help='d_model of the base model.')
+    parser.add_argument('--no_warning', action='store_true', default=False)
+    parser.add_argument('--coord_check_norm', type=str, default=None)
+
 
 
     args = parser.parse_args()
 
-    print(args)
+    if args.no_warning:
+        import warnings;   warnings.filterwarnings("ignore")
+
 
     # Set the random seed manually for reproducibility.
     torch.manual_seed(args.seed)
@@ -246,8 +252,8 @@ if __name__ == '__main__':
 
     device = args.device = torch.device("cuda" if args.cuda else "cpu")
 
-    # torch.set_default_device('cuda:0')
-    # print(device, torch.get_default_device())
+    torch.set_default_device('cuda:0')
+    print(device, torch.get_default_device())
 
     ###############################################################################
     # Load data
@@ -352,8 +358,8 @@ if __name__ == '__main__':
         import os
         os.makedirs('coord_checks', exist_ok=True)
         plotdir = 'coord_checks'
-        torch.set_default_device('cuda:0')
-        coord_check(mup=True, lr=args.lr, optimizer=args.optimizer, batch_size=args.batch_size, nsteps=args.coord_check_nsteps, nseeds=args.coord_check_nseeds, data_dir=args.data, args=args, plotdir=plotdir, legend=False)
+        # torch.set_default_device('cuda:0')
+        coord_check(mup=True, lr=args.lr, optimizer=args.optimizer, batch_size=args.batch_size, nsteps=args.coord_check_nsteps, nseeds=args.coord_check_nseeds, data_dir=args.data, args=args, plotdir=plotdir, legend=True)
         # coord_check(mup=False, lr=args.lr, optimizer=args.optimizer, batch_size=args.batch_size, nsteps=args.coord_check_nsteps, nseeds=args.coord_check_nseeds, data_dir=args.data, args=args, plotdir=plotdir, legend=False)
         import sys; sys.exit()
 

examples/SSMs/mixer_seq_simple.py
@@ -141,15 +141,15 @@ class MixerModelEmbedding(nn.Module):
         # NOTE: we initialize embed_w to be of shape (fan_in, fan_out), whereas
         # we initialize decode_w in MixerModelDecoder to be of shape (fan_out, fan_in).
         # The reason is to accommodate what F.embedding expects
-        if 'mup' in hyperparam_mode:
+        if 'umap' in hyperparam_mode:
+            self.embed_w = nn.Parameter(torch.randn(fan_in, fan_out))
+        elif 'mup' in hyperparam_mode:
             self.embed_w = nn.Parameter(torch.randn(fan_in, fan_out) * (width_mult**(-.5)))
         elif 'sp' in hyperparam_mode:
             self.embed_w = nn.Parameter(torch.randn(fan_in, fan_out))
         elif 'ntk' in hyperparam_mode:
             self.embed_w = nn.Parameter(torch.randn(fan_in, fan_out))
         elif 'mf' in hyperparam_mode:
-            self.embed_w = nn.Parameter(torch.randn(fan_in, fan_out))
-        elif 'umap' in hyperparam_mode:
             self.embed_w = nn.Parameter(torch.randn(fan_in, fan_out))
         else:
             raise ValueError(f'hyperparam_mode = {hyperparam_mode} not recognized.')
@@ -162,17 +162,27 @@ class MixerModelEmbedding(nn.Module):
 
         output should have the form (batch_size, embedding_dim, sequence_length)
         """
-        # x_onehot = F.one_hot(x, num_classes=self.vocab_size) # size (seq_length, batch_size, vocab_size)
-        embeded = F.embedding(x, self.embed_w) # size (seq_length, batch_size, embedding_dim)
-        embeded = einops.rearrange(embeded, 'l b e -> b e l')
+        # TODO: Stud to effectively decrease token size:
+        # breakpoint()
+        with torch.no_grad():
+            MAX_TOKEN_SIZE = 20
+            self.embed_w[MAX_TOKEN_SIZE:, :] = 0.
+        
+        x_onehot = F.one_hot(x, num_classes=self.vocab_size).to(self.embed_w.dtype) # size (seq_length, batch_size, vocab_size)
+        embeded = torch.einsum('lbv,vd->bdl', x_onehot, self.embed_w)
 
-        if 'mup' in self.hyperparam_mode:
+
+        # embeded = F.embedding(x, self.embed_w) # size (seq_length, batch_size, embedding_dim)
+        # embeded = einops.rearrange(embeded, 'l b e -> b e l')
+        # breakpoint()
+
+        if 'umap' in self.hyperparam_mode:
+            return embeded
+        elif 'mup' in self.hyperparam_mode:
             return embeded*(self.width_mult**(.5))
         elif 'sp' in self.hyperparam_mode:
             return embeded
         elif 'ntk' in self.hyperparam_mode:
-            return embeded
-        elif 'mf' in self.hyperparam_mode:
             return embeded
         elif 'mf' in self.hyperparam_mode:
             return embeded
@@ -186,15 +196,16 @@ class MixerModelDecoder(nn.Module):
         self.width_mult = width_mult
         self.hyperparam_mode = hyperparam_mode
         self.fan_in = fan_in # Used for u-mup
-        if 'mup' in hyperparam_mode:
-            self.decode_w = nn.Parameter(torch.randn(fan_out, fan_in)) #*(width_mult**(-.5))) #TODO: Consider temporary comment out the multiplier.
+        if 'umup' in hyperparam_mode:
+            self.decode_w = nn.Parameter((torch.randn(fan_out, fan_in)))
+        elif 'mup' in hyperparam_mode:
+            self.decode_w = nn.Parameter((torch.randn(fan_out, fan_in)))# *(width_mult**(-.5))) #TODO comment out for now
+            # self.decode_w = nn.Parameter((torch.rand(fan_out, fan_in)*2) - 1)
         elif 'sp' in hyperparam_mode:
             self.decode_w = nn.Parameter((torch.randn(fan_out, fan_in)*(width_mult**(-.5))))
         elif 'ntk' in hyperparam_mode:
             self.decode_w = nn.Parameter((torch.randn(fan_out, fan_in)))
         elif 'mf' in hyperparam_mode:
-            self.decode_w = nn.Parameter((torch.randn(fan_out, fan_in)))
-        elif 'umup' in hyperparam_mode:
             self.decode_w = nn.Parameter((torch.randn(fan_out, fan_in)))
         else:
             raise ValueError(f'hyperparam_mode = {hyperparam_mode} not recognized.')
@@ -204,16 +215,16 @@ class MixerModelDecoder(nn.Module):
         output has shape (sequence_length, batch_size, vocab_size)
         """
         x_t = einops.rearrange(x, "b h l -> b l h")
-        if 'mup' in self.hyperparam_mode:
-            return torch.einsum('vh,blh->lbv', self.decode_w, x_t)*(self.width_mult**(-.5))
+        if 'umup' in self.hyperparam_mode:
+            return torch.einsum('vh,blh->lbv', self.decode_w, x_t)*(self.fan_in**(-1.0))
+        elif 'mup' in self.hyperparam_mode:
+            return torch.einsum('vh,blh->lbv', self.decode_w, x_t)*(self.width_mult**(-.5))/ 2**1.5 # TODO Added the division by constant arbitrarily
         elif 'sp' in self.hyperparam_mode:
-            return torch.einsum('vh,blh->lbv', self.decode_w, x_t)
+            return torch.einsum('vh,blh->lbv', self.decode_w, x_t) / 2**1.5 # TODO Added the division by constant arbitrarily
         elif 'ntk' in self.hyperparam_mode:
             return torch.einsum('vh,blh->lbv', self.decode_w, x_t)*(self.width_mult**(-.5))
         elif 'mf' in self.hyperparam_mode:
             return torch.einsum('vh,blh->lbv', self.decode_w, x_t)*(self.width_mult**(-1.0))
-        elif 'umup' in self.hyperparam_mode:
-            return torch.einsum('vh,blh->lbv', self.decode_w, x_t)*(self.fan_in**(-1.0))
         else:
             raise ValueError
 
@@ -306,28 +317,30 @@ class MixerModelWithSimpleHead(nn.Module):
         }
 
     def forward(self, input_ids, inference_params=None, **mixer_kwargs):
+        # breakpoint()
         hidden_states = self.embedding(input_ids)
+        # breakpoint()
         residual = None
-        for layer in self.layers:
+        for li, layer in enumerate(self.layers):
             hidden_states, residual = layer(
                 hidden_states, None, inference_params=inference_params, **mixer_kwargs # Note: can change residual to None to get rid of residuals
             )
-        if (not hasattr(self, 'fused_add_norm')) or (not self.fused_add_norm):
-            residual = (hidden_states + residual) if residual is not None else hidden_states
-            hidden_states = residual # NOTE: Got rid of norm_f here
-        else:
-            raise ValueError
-            # Set prenorm=False here since we don't need the residual
-            hidden_states = layer_norm_fn(
-                hidden_states,
-                self.norm_f.weight,
-                self.norm_f.bias,
-                eps=self.norm_f.eps,
-                residual=residual,
-                prenorm=False,
-                residual_in_fp32=self.residual_in_fp32,
-                is_rms_norm=isinstance(self.norm_f, RMSNorm)
-            )
+        # if (not hasattr(self, 'fused_add_norm')) or (not self.fused_add_norm):
+        #     residual = (hidden_states + residual) if residual is not None else hidden_states
+        #     hidden_states = residual # NOTE: Got rid of norm_f here
+        # else:
+        #     raise ValueError
+        #     # Set prenorm=False here since we don't need the residual
+        #     hidden_states = layer_norm_fn(
+        #         hidden_states,
+        #         self.norm_f.weight,
+        #         self.norm_f.bias,
+        #         eps=self.norm_f.eps,
+        #         residual=residual,
+        #         prenorm=False,
+        #         residual_in_fp32=self.residual_in_fp32,
+        #         is_rms_norm=isinstance(self.norm_f, RMSNorm)
+        #     )
         # breakpoint()
         output = self.decoder(hidden_states)
         return output

mup/coord_check.py
@@ -307,8 +307,10 @@ def _get_coord_data(models, dataloader, optcls, nsteps=3,
         from tqdm import tqdm
         pbar = tqdm(total=nseeds * len(models))
 
+
+    GRAD_PLOT_DIR = '/home/berlin/mup/examples/SSMs/grad_plot'
+    os.makedirs(GRAD_PLOT_DIR, exist_ok=True)
     for i in seeds_iter:
-        # print(f"seed is set to {i}")
         torch.manual_seed(i)
         for width, model in models.items():
             # if width == 1700:
@@ -321,6 +323,7 @@ def _get_coord_data(models, dataloader, optcls, nsteps=3,
 
             optimizer = optcls(model)
             for batch_idx, batch in enumerate(dataloader, 1):
+                # print(f"batch_idx is {batch_idx}")
                 remove_hooks = []
                 # add hooks
                 for name, module in model.named_modules():
@@ -345,6 +348,7 @@ def _get_coord_data(models, dataloader, optcls, nsteps=3,
                     if flatten_input:
                         data = data.view(data.size(0), -1)
                     # breakpoint()
+
                     output = model(data)
                     if flatten_output:
                         output = output.view(-1, output.shape[-1])
@@ -365,6 +369,23 @@ def _get_coord_data(models, dataloader, optcls, nsteps=3,
                         raise NotImplementedError(f'unknown `lossfn`: {lossfn}')
                 optimizer.zero_grad()
                 loss.backward()
+
+                # if i == 0:
+                #     file_name = f'inproj_t={batch_idx}_w={width}_seed={i}.pt'
+                #     grad = model.layers[0].mixer.in_proj.weight.grad
+                #     torch.save(grad, os.path.join(GRAD_PLOT_DIR, file_name))
+                # breakpoint()
+
+                # if i == 0 and batch_idx == 1:
+                #     breakpoint()
+
+                # model.layers[0].mixer.in_proj.weight.grad[...] = torch.sign(model.layers[0].mixer.in_proj.weight.grad[...])#1.
+                # assert 0. not in model.layers[0].mixer.in_proj.weight.grad[...]
+                
+                # model.layers[0].mixer.B.weight.grad[...] = 0.
+                # assert 0. not in model.layers[0].mixer.B.weight.grad[...] 
+
+                # breakpoint()
                 # if batch_idx == 1:
                 #     print(f"DEBUG: \t d_model = {model.h} \t\t l1 = {FDICT['l1'](model.up_project.up_project.grad)*(model.h**2.0)}") # Temporary debug print (Aug 8, 2024)
                 optimizer.step()
@@ -497,34 +518,39 @@ def get_coord_data(models, dataloader, optimizer='sgd', lr=None, mup=True,
         return params
     from examples.SSMs.ssm import SSM, SelectiveSSMKernel, NonSelectiveSSMKernel, AdamSSM, MuSGD as SGDSSM
     from examples.SSMs.mixer_seq_simple import MixerModelWithSimpleHead
-    if optimizer == 'sgd':
-        def optcls(model):
-            if isinstance(model, SSM):
-                model_names = []
-                for n, _ in model.named_parameters():
-                    model_names.append(n)
-                if isinstance(model.kernel, SelectiveSSMKernel):
-                    return SGDSSM(get_trainable(model), lr=lr, ssm_force_multiply=model.h**(-1.0), model_names=model_names, L=1024//(8**2),)
-                elif isinstance(model.kernel, NonSelectiveSSMKernel):
-                    return SGDSSM(get_trainable(model), lr=lr, ssm_force_multiply=1, model_names=model_names, L=1024//(8**2),)
-                else:
-                    raise ValueError
-            else:
-                return SGD(get_trainable(model), lr=lr)
+    def optcls(model):
+        model_names = []
+        for n, _ in model.named_parameters():
+            model_names.append(n)
+        return AdamSSM(get_trainable(model), hyperparam_mode=hyperparam_mode, model_names=model_names, lr=lr,)
+    # if optimizer == 'sgd':
+    #     def optcls(model):
+    #         if isinstance(model, SSM):
+    #             model_names = []
+    #             for n, _ in model.named_parameters():
+    #                 model_names.append(n)
+    #             if isinstance(model.kernel, SelectiveSSMKernel):
+    #                 return SGDSSM(get_trainable(model), lr=lr, ssm_force_multiply=model.h**(-1.0), model_names=model_names, L=1024//(8**2),)
+    #             elif isinstance(model.kernel, NonSelectiveSSMKernel):
+    #                 return SGDSSM(get_trainable(model), lr=lr, ssm_force_multiply=1, model_names=model_names, L=1024//(8**2),)
+    #             else:
+    #                 raise ValueError
+    #         else:
+    #             return SGD(get_trainable(model), lr=lr)
 
-    elif optimizer == 'adam':
-        def optcls(model):
-            if isinstance(model, SSM) or isinstance(model, MixerModelWithSimpleHead):
-                model_names = []
-                for n, _ in model.named_parameters():
-                    model_names.append(n)
-                return AdamSSM(get_trainable(model), hyperparam_mode=hyperparam_mode, model_names=model_names, lr=lr,)
-            else:
-                return Adam(get_trainable(model), lr=lr)
-    elif optimizer == 'adamw':
-        optcls = lambda model: AdamW(get_trainable(model), lr=lr)
-    elif optimizer is None:
-        raise ValueError('optimizer should be sgd|adam|adamw or a custom function')
+    # elif optimizer == 'adam':
+    #     def optcls(model):
+    #         if isinstance(model, SSM) or isinstance(model, MixerModelWithSimpleHead):
+    #             model_names = []
+    #             for n, _ in model.named_parameters():
+    #                 model_names.append(n)
+    #             return AdamSSM(get_trainable(model), hyperparam_mode=hyperparam_mode, model_names=model_names, lr=lr,)
+    #         else:
+    #             return Adam(get_trainable(model), lr=lr)
+    # elif optimizer == 'adamw':
+    #     optcls = lambda model: AdamW(get_trainable(model), lr=lr)
+    # elif optimizer is None:
+    #     raise ValueError('optimizer should be sgd|adam|adamw or a custom function')
     
     data = _get_coord_data(models, dataloader, optcls, **kwargs)
     data['optimizer'] = optimizer